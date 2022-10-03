f513d7ae341471f88dbd06a15a525bda3a9b9b36
A.Karnatski <kapter@gmail.com>
2022-10-03 20:58:08 +0300

    хранит токены прогноза погоды

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-open_weather_token = "9f2bcc1a11965144bd61703c6c0ec0f8"
-tg_bot_token = "5546034139:AAEkn6khDBbLALngz6pLR7WJS3PX8GI8r8o" #@test324234324_bot
+open_weather_token = ""
+tg_bot_token = "" #@test324234324_bot
